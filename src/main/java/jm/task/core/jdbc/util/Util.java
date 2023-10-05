9b236279954900b45ecc8e472e06a00f9e0ba08a
Artem <142834875+bzrktm@users.noreply.github.com>
2023-10-05 23:13:51 +0300

    Realization

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/forprepro";
+    public static final String LOGIN = "root";
+    public static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        Connection connection;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
 }
