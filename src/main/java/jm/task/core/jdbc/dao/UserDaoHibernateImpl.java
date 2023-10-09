eba3adc942801e0a4dc1c7006f9a3be238a3be7d
Artem <142834875+bzrktm@users.noreply.github.com>
2023-10-09 11:37:29 +0300

    Realization Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class UserDaoHibernateImpl implements UserDao {
 
     private final SessionFactory sessionFactory = Util.getSessionFactory();
+
     public UserDaoHibernateImpl() {
 
     }
